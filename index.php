5e6c7ed1c0d5369d5cde1013f6a5d598583afb6a
Mac Doussias <mdoussias@csumb.edu>
2018-06-24 14:18:29 +0000

    added links for all labs and homework

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
-<html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
+<html>
+  <body>
 
-echo 'Mac Doussias CST336 Internet Programming';
-?>
-
-  <br /><br />
-    <h1>Labs</h1>
+  <?php
+  echo 'Mac Doussias CST336 Internet Programming';
+  ?>
+    <br /><br />
+    <h2>LABS</h2>
     <a href="labs/lab1"> Lab 1 </a>
-    <h1>Homework</h1>
+    <a href="labs/lab2"> Lab 2 </a>    
+    <a href="labs/lab3"> Lab 3 </a>
+    <a href="labs/lab4"> Lab 4 </a>
+    <a href="labs/lab5"> Lab 5 </a>
+    <a href="labs/lab6"> Lab 6 </a>
+    <a href="labs/lab7"> Lab 7 </a>    
+    <a href="labs/lab8"> Lab 8 </a>
+    
+    <h2>HOMEWORK</h2>
     <a href="homework/homework1"> Homework1 </a>
-</body>
+    <a href="homework/homework2"> Homework2 </a>
+    <a href="homework/homework3"> Homework3 </a>
+    <a href="homework/homework4"> Homework4 </a>
+    <a href="homework/homework5"> Homework5 </a>
+    <a href="homework/homework6"> Homework6 </a>
+    <a href="homework/homework7"> Homework7 </a>
+    <a href="homework/homework8"> Homework8 </a>    
+  </body>
 </html>
