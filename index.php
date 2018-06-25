83ab0c24fe465fed218ca5facafc2f955923a45b
Mac Doussias <mdoussias@csumb.edu>
2018-06-25 04:45:08 +0000

    Update to add line breaks between links

--- index.php
+++ index.php
@@ -6,23 +6,23 @@
   ?>
     <br /><br />
     <h2>LABS</h2>
-    <a href="labs/lab1"> Lab 1 </a>
-    <a href="labs/lab2"> Lab 2 </a>    
-    <a href="labs/lab3"> Lab 3 </a>
-    <a href="labs/lab4"> Lab 4 </a>
-    <a href="labs/lab5"> Lab 5 </a>
-    <a href="labs/lab6"> Lab 6 </a>
-    <a href="labs/lab7"> Lab 7 </a>    
-    <a href="labs/lab8"> Lab 8 </a>
+    <a href="labs/lab1"> Lab 1 </a> <br />
+    <a href="labs/lab2"> Lab 2 </a> <br />    
+    <a href="labs/lab3"> Lab 3 </a> <br />
+    <a href="labs/lab4"> Lab 4 </a> <br />
+    <a href="labs/lab5"> Lab 5 </a> <br /> 
+    <a href="labs/lab6"> Lab 6 </a> <br />
+    <a href="labs/lab7"> Lab 7 </a> <br />    
+    <a href="labs/lab8"> Lab 8 </a> <br />
     
     <h2>HOMEWORK</h2>
-    <a href="homework/homework1"> Homework1 </a>
-    <a href="homework/homework2"> Homework2 </a>
-    <a href="homework/homework3"> Homework3 </a>
-    <a href="homework/homework4"> Homework4 </a>
-    <a href="homework/homework5"> Homework5 </a>
-    <a href="homework/homework6"> Homework6 </a>
-    <a href="homework/homework7"> Homework7 </a>
-    <a href="homework/homework8"> Homework8 </a>    
+    <a href="homework/homework1"> Homework1 </a> <br />
+    <a href="homework/homework2"> Homework2 </a> <br />
+    <a href="homework/homework3"> Homework3 </a> <br />
+    <a href="homework/homework4"> Homework4 </a> <br />
+    <a href="homework/homework5"> Homework5 </a> <br />
+    <a href="homework/homework6"> Homework6 </a> <br />
+    <a href="homework/homework7"> Homework7 </a> <br />
+    <a href="homework/homework8"> Homework8 </a> <br />    
   </body>
 </html>
