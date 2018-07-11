728cfcc51ca06c8b4aa2be21baf4edf7da906eaf
Mac Doussias <mdoussias@csumb.edu>
2018-07-11 04:45:06 +0000

    Adding functionality to connect to Heroku automatically

--- labs/lab5/dbConnection.php
+++ labs/lab5/dbConnection.php
@@ -9,6 +9,15 @@
 		$username = 'root';
 		$password = '';
 		
+		//when connecting from Heroku
+		if (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) { 
+			$url = parse_url(getenv("CLEARDB_DATABASE_URL")); $host = $url["host"];
+			$dbname = substr($url["path"], 1);
+			$username = $url["user"];
+			$password = $url["pass"]; 
+			
+		}
+	
 		//Creates a db connection
 		$dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 		
